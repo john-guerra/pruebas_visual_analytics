9700b5736686720edf43e78cb72d0693265efcd2
John Alexis Guerra Gómez <john.guerra@gmail.com>
2016-11-08 20:50:10 -0500

    Serving index.html as default

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index.html');
 });
 
 app.set('port', (process.env.PORT || 5000));

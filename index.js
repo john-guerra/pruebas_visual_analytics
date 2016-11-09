6360b9b61668e48a487dce1608c9c9d1bdf3ef08
John Alexis Guerra Gómez <john.guerra@gmail.com>
2016-11-08 21:00:46 -0500

    Sending json as url

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index.html');
+});
+
+app.get('/getData', function (req, res) {
+
+	// Connect to db
+  res.sendFile(path.normalize(__dirname + '/ieeevisNetwork.json'));
 });
 
 app.set('port', (process.env.PORT || 5000));
